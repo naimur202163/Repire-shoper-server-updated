13063ae85daf8a65a5a21853dc826a3332beb4d2
unknown <you@example.com>
2021-11-27 12:51:22 +0600

    admin added

--- index.js
+++ index.js
@@ -60,6 +60,19 @@ async function run() {
 
 
         });
+
+        // Geting user Admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.send({ admin: isAdmin })
+        })
+
         // Adding products from Single Page
         app.post('/userProducts', async (req, res) => {
             const products = req.body;
